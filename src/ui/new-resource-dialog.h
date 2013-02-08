597739271034cb62e23b53cddb0135dd4aea9623
tiesselune <julien_sosthene@yahoo.co.uk>
2013-02-08 15:25:04 +0100

    Documented NewResourceDialog

--- src/ui/new-resource-dialog.h
+++ src/ui/new-resource-dialog.h
@@ -17,6 +17,15 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * \file new-resource-dialog.h
+ * \brief Dialog for creating resources, header
+ * \author Julien Sosthène
+ *
+ * Header of the dialog for creating resources like books or chapters.
+ *
+ */
+
 #ifndef _NEW_RESOURCE_DIALOG_H_
 #define _NEW_RESOURCE_DIALOG_H_
 #include <glibmm/ustring.h>
@@ -25,10 +34,16 @@
 #include <gtkmm/entry.h>
 #include <gtkmm/textview.h>
 #include <gtkmm/stock.h>
-#include "entities/section.h"
-#include <iostream>
 #include <gtkmm/scrolledwindow.h>
 
+#include "entities/section.h"
+
+/**
+ * \enum SpecificStrings
+ * \brief String types
+ *
+ * Convenience enumeration for getting specific strings in an array.
+ */
 enum SpecificStrings{
 	NEWRESOURCE,
 	NAMEINFO,
@@ -39,26 +54,60 @@ enum SpecificStrings{
 	
 };
 
+
+/** \class NewResourceDialog
+ * \brief Dialog for creating resources
+ * 
+ * Dialog for creating new resources as books, chapters, or characters.
+ * 
+ */
 class NewResourceDialog: public Gtk::Dialog
 {
-public:
-	NewResourceDialog(int type, 
-	                  Gtk::Widget* additionnalWidget,
-	                  Gtk::Window* parent);
-	Glib::ustring getName();
-	Glib::ustring getDescription();
-	void setName(Glib::ustring name);
-	void setDescription(Glib::ustring desc);
-protected:
+	public:
+		/** \brief Constructor
+		 * Initializes a new NewResourceDialog.
+		 *
+		 * \param[in] type The type of the entity to create.
+		 * \param[in] parent Parent window to this dialog
+		 */
+		NewResourceDialog(int type, Gtk::Window* parent);
 
-private:
-	Glib::RefPtr<Gtk::TextBuffer> descBuffer;
-	Glib::ustring getSpecificString(int type, int specString);
-	Gtk::Widget* getSpecificWidget(int type);
-	Gtk::Box* dialogVBox;
-	Gtk::Entry* nameEntry;
-	Gtk::TextView* descEntry;
+		/** \brief Gets the name of new resource
+		 * 
+		 * \return The string contained in the first entry
+		 */
+		Glib::ustring getName();
+
+		/** \brief Gets the description of new resource
+		 * 
+		 * \return The string contained in the second entry
+		 */
+		Glib::ustring getDescription();
+
+		/** \brief Sets the name of resource
+		 * 
+		 * \param[in] name the name to replace the former one.
+		 */
+		void setName(Glib::ustring name);
+		
+		/** \brief Sets the description of resource
+		 * 
+		 * \param[in] desc the description to replace the former one.
+		 */
+		void setDescription(Glib::ustring desc);
+	protected:
+
+	private:
+		Gtk::Box* dialogVBox ; /**<Gtk::VBox for creating the dialog*/
+		Gtk::Entry* nameEntry; /**<Gtk::Entry for typing the name of the resource*/
+		Gtk::TextView* descEntry; /**<Gtk::TextView for typing the description of the resource*/
+
+		/** \brief Gets a string specific to a resource
+		 * 
+		 * \param[in] type the type of the resources
+		 * \param[in] specString the SpecificStrings enum element
+		 */
+		Glib::ustring getSpecificString(int type, int specString);
 };
 
 #endif // _NEW_RESOURCE_DIALOG_H_
-
